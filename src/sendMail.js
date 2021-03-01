eea10147d88cb92b6a631b00ea9cebd01818220a
Andrea Trigg <aetrigg@iu.edu>
2021-03-01 18:37:02 -0500

    updated sendmail.js so mail is coming from a different email

--- src/sendMail.js
+++ src/sendMail.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const sendMailRouter = express.Router();
 const nodemailer = require('nodemailer');
-require('dotenv').config();
 
 module.exports = sendMailRouter;
 
@@ -35,74 +34,24 @@ sendMailRouter.post('/', (req, res, next) => {
   var email = req.body.email
   var subject = req.body.subject
   var message = req.body.message
-  var content = `New Message from Andrea Designs!
-From: ${name} at ${email}
-Message: ${message}`
+  var content = `New Message from Andrea Designs! From: ${name} at ${email} Message: ${message}`
 
   var mail = {
-    from: 'hello@designsbyandrea.me',
+    from: 'formresponses@netlify.com',
     to: 'hello@designsbyandrea.me',
     subject: subject,
     text: content
-  }
-  
+  };
+
   transporter.sendMail(mail, (err,data) => {
-    if(err) {
+    if(err){
       res.json({
         status: 'fail'
       })
     } else {
       res.json({
-        status: 'success'
+        status: 'sucess'
       })
     }
   })
 });
-
-/*const nodemailer = require('nodemailer');
-require('dotenv').config();
-
-exports.handler = function(event, context, callback) {
-
-  let data = JSON.parse(event.body)
-
-    let transporter = nodemailer.createTransport({
-        host:'smtp.outlook.com',
-        port:587,
-        auth:{
-          user:process.env.THE_EMAIL,
-          pass:process.env.THE_PASSWORD
-        }
-    });
-
-    transporter.verify((error, success) => {
-      if(error) {
-        //if error happened code ends here
-        console.error(error)
-      } else {
-        //this means success
-        console.log('transport was successful, ')
-      }
-    });
-
-    transporter.sendMail({
-      from: 'hello@designsbyandrea.me',
-      to: 'hello@designsbyandrea.me',
-      subject: `${data.subject}`,
-      html: `
-        <h3>New Email from ${data.name} <a href="mailto:${data.email}>">${data.email}</a></h3>
-        <br/>
-        <p>${data.message}</p>
-      `
-    }, function(error, info){
-      if(error){
-        callback(error);
-      }else{
-        callback(null, {
-          statusCode:200,
-          
-        });
-      };
-    });
-    
-};*/
