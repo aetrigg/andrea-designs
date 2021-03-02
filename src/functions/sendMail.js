cbce10805f9559e8d2482b2286b7a1187f14a173
Andrea Trigg <aetrigg@iu.edu>
2021-03-01 19:49:28 -0500

    still attempting to update sendmail

---
+++ src/functions/sendMail.js
@@ -0,0 +1,34 @@
+const nodemailer = require('nodemailer');
+
+exports.handler = function(event, context, callback) {
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.outlook.com',
+        port: 587,
+        auth: {
+            user: process.env.THE_EMAIL,
+            password: process.env.THE_PASSWORD
+        }
+    });
+    console.log(event.body);
+    
+    var name = req.body.name
+    var email = req.body.email
+    var subject = req.body.subject
+    var message = req.body.message
+    var content = `New Message from Andrea Designs! From: ${name} at ${email} Message: ${message}`
+
+    transporter.sendMail({
+        from: 'hello@designsbyandrea.me',
+        to: 'hello@designsbyandrea.me',
+        subject: subject,
+        text: content
+    }, function(error, info) {
+    	if (error) {
+    		callback(error);
+    	} else {
+    		callback(null, {
+			    statusCode: 200
+	    	});
+    	}
+    });
+}
